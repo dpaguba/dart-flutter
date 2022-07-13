3d29b87f9a7b91ac86c9eb5bac70f4d5313ab918
Dmytro Pahuba <dpahuba@MacBook-Air-Dmytro.local>
2022-07-13 10:16:31 +0200

    learned Widget - Container, Text

---
+++ my_app/lib/presentation/widgets_examples/widgets/container_text_example.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class ContainerTextExample extends StatelessWidget {
+  const ContainerTextExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 180,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          border: Border.all(
+            color: Colors.white,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+          child: Material(
+            borderRadius: BorderRadius.circular(10),
+            // shadows
+            elevation: 10,
+            child: Container(
+              alignment: Alignment.center,
+              // margin: EdgeInsets.only(bottom: 20),
+              height: 100,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Text(
+                "Hello User! How are you?",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  fontFamily: "Rubik",
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- my_app/lib/presentation/widgets_examples/widgets_example_page.dart
+++ my_app/lib/presentation/widgets_examples/widgets_example_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/presentation/widgets_examples/widgets/container_text_example.dart';
 
 class WidgetsExamplesPage extends StatelessWidget {
   const WidgetsExamplesPage({Key? key}) : super(key: key);
@@ -11,24 +12,22 @@ class WidgetsExamplesPage extends StatelessWidget {
 
       // AppBar
       appBar: AppBar(
+        // Styles for AppBar
+        // another property to center the title in appBar
+        centerTitle: true,
+        backgroundColor: Colors.blue[200],
+
         // Create leading button
         leading: const Icon(
           Icons.home,
           size: 40,
         ),
-        // another property to center the title in appBar
-        // centerTitle: true,
 
-        // Styles for AppBar
-        backgroundColor: Colors.blue[200],
-
-        title: const Center(child: Text("Widgets Example")),
+        title: const Text("Widgets Example"),
       ),
 
       // Body
-      body: const Center(
-        child: Text("Hello World!"),
-      ),
+      body: const ContainerTextExample(),
     );
   }
 }
