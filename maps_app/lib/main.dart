1cd30fbc066b3d427a872ea90a01c15a1f883803
Dmytro Pahuba <dpahuba@MacBook-Air-Dmytro.local>
2022-07-18 10:52:53 +0200

    init

---
+++ maps_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MapApp(),
+    );
+  }
+}
+
+class MapApp extends StatefulWidget {
+  // const MapApp({Key? key}) : super(key: key);
+
+  @override
+  State<MapApp> createState() => _MapAppState();
+}
+
+class _MapAppState extends State<MapApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
